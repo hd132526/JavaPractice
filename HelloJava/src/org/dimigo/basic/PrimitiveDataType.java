df0c09e6c1f1ab29734499abce5ca3c7ad27d40d
FrostisLee <FrostisLee@Frostis>
2015-03-19 11:50:23 +0900

    123

---
+++ HelloJava/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,12 @@
+package org.dimigo.basic;
+
+
+public class PrimitiveDataType {
+	public static void main(String[] args) {
+		int b3 = 128;
+		System.out.println("byte 범위 : " + Byte.MAX_VALUE);
+		double a = 1.04;
+		System.out.println("byte 범위 " + a);
+	}
+
+}
