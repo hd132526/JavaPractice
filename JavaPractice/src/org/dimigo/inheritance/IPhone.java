a506c9cd8c5dad286f5efec83213acdd62efb31c
FrostisLee <FrostisLee@Frostis>
2015-08-31 12:03:58 +0900

    1

---
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+	JavaClass 
+	-org.dimigo.inheritance 
+	--IPhone.java
+	@date : 2015. 8. 25.
+	@author : 이승현
+	@version : 1.0
+ */
+public class IPhone extends SmartPhone {
+	public IPhone() {
+		
+	}
+	
+	public IPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void pay() {
+		System.out.println("애플 페이로 결제합니다.");
+	}
+	
+	public void useAirDrop() {
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+	JavaClass 
+	-org.dimigo.inheritance 
+	--SmartPhone.java
+	@date : 2015. 8. 25.
+	@author : 이승현
+	@version : 1.0
+ */
+public class SmartPhone {
+	String model;
+	String company;
+	int price;
+	
+	public SmartPhone() {
+		
+	}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public void pay() {
+		
+	}
+	
+	public void useSpecialFunction(SmartPhone phone) {
+		if(phone instanceof IPhone) {
+			IPhone i = (IPhone)phone;
+			i.useAirDrop();
+		}
+		if(phone instanceof Galaxy) {
+			Galaxy g = (Galaxy)phone;
+			g.useWirelessCharging();
+		}
+		
+	}
+	
+	public String toString() {
+		return "모델명 : " + model + " 제조사 : " + company + " 가격 : " + price;
+	}
+}
