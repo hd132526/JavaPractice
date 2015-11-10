cfd81a3aae77051a7197607c912c37b0a19e9f98
FrostisLee <FrostisLee@Frostis>
2015-11-10 10:51:11 +0900

    20151110

---
+++ JavaPractice/src/org/dimigo/thread/TaskThread.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+	JavaPractice 
+	-org.dimigo.thread 
+	--TaskThread.java
+	@date : 2015. 11. 4.
+	@author : 이승현
+	@version : 1.0
+ */
+public class TaskThread extends Thread {
+	
+}
